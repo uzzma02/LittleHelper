fc2f8a32bca2d7d7300a7d45025fcce5800a26c6
a2-uzzma02 <uzzma02@gmail.com>
2020-02-13 10:23:02 -0800

    Changed Apple Store to App store

--- littlehelperreact/src/AppDownload.js
+++ littlehelperreact/src/AppDownload.js
@@ -5,7 +5,7 @@ const AppDownload = ()=> {
         //---- Call to Action Section ----
         <section className="page-section bg-secondary text-white">
             <div className="container text-center">
-            <h2 className="mb-4">Look for us in the Apple Store & Google Play Store</h2>
+            <h2 className="mb-4">Look for us in the App Store & Google Play Store</h2>
             <a className="btn btn-light btn-xl" href="https://startbootstrap.com/themes/creative/">Download Now!</a>
             </div>
         </section>
